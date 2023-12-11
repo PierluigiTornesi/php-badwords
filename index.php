b754ac9f3fc1c6a3a14f5276fc7b48581b5d332b
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-12-11 15:30:49 +0100

    add: aggiunti commenti

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Badwords</title>
+    </head>
+    <body>
 
-    <h1>Inserisci un paragrafo e una parola da censurare</h1>
+        <h1>Inserisci un paragrafo e una parola da censurare</h1>
 
-    <form action="censured.php" method="GET">
-        <textarea name="paragraph" id="paragraph" cols="60" rows="10" placeholder="Inserisci un paragrafo"></textarea>
+        <!-- Form per far inserire un paragrafo e una parola da censurare all'interno di quest'ultimo-->
+        <form action="censured.php" method="GET">
+            <!-- textarea per far inserire il paragrafo -->
+            <textarea name="paragraph" id="paragraph" cols="60" rows="10" placeholder="Inserisci un paragrafo"></textarea>
 
-        <br>
+            <br>
 
-        <label for="badword">Inserisci una parola da censurare nel paragrafo</label>
-        <input type="text" id="badword" name="badword">
+            <!-- input text per far inserire la parola -->
+            <label for="badword">Inserisci una parola da censurare nel paragrafo</label>
+            <input type="text" id="badword" name="badword">
 
-        <br>
+            <br>
 
-        <button type="submit">Invia</button>
-    </form>
+            <!-- bottone per inviare i dati -->
+            <button type="submit">Invia</button>
+        </form>
 
 
-</body>
+    </body>
 </html>
